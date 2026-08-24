1e746626850cc0110363f405e11ee8b1cc753661
ikeepsz <dev.rafapinto@gmail.com>
2026-08-23 23:13:09 -0300

    fix: load bronze file fixed

--- infra/aws/lambda_handler.py
+++ infra/aws/lambda_handler.py
@@ -1,1 +1,43 @@
-def lambda_handler
+import boto3
+import json
+from datetime import datetime
+from zoneinfo import ZoneInfo
+
+sp_zoneinfo = ZoneInfo("America/Sao_Paulo")
+ingested_at = datetime.now(sp_zoneinfo)
+
+source_file = 'products/2026/08/14/20260814_005126.json'
+s3 = boto3.client('s3')
+response = s3.get_object(Bucket='medallion-retail-raw-rafa', Key=source_file)
+
+json_data = json.load(response['Body'])
+item = json_data['products'][0]
+
+
+row = (
+    item['id'],
+    item['title'],
+    item['description'],
+    item['category'],
+    item['price'],
+    item['discountPercentage'],
+    item['rating'],
+    item['stock'],
+    json.dumps(item['tags']),
+    item['brand'],
+    item['sku'],
+    json.dumps(item['dimensions']),
+    item['warrantyInformation'],
+    item['shippingInformation'],
+    item['availabilityStatus'],
+    json.dumps(item['reviews']),
+    item['returnPolicy'],
+    item['minimumOrderQuantity'],
+    json.dumps(item['meta']),
+    json.dumps(item['images']),
+    item['thumbnail'],
+    source_file,
+    ingested_at
+)
+
+print(tuple(item))
--- src/load/load_bronze.py
+++ src/load/load_bronze.py
@@ -1,56 +1,30 @@
-import logging
-import json
-import psycopg2
 from pathlib import Path
+import psycopg2
+import logging
 
 BASE_DIR = Path(__file__).resolve().parents[2]
-json_path = BASE_DIR / 'data' / 'raw' / 'products_20260807_1624.json'
-
 LOG_DIR = BASE_DIR / 'logs'
 LOG_DIR.mkdir(exist_ok=True)
 
 logging.basicConfig(level=logging.INFO, 
                     format="%(asctime)s - %(message)s",
                     filename=LOG_DIR / 'app.log',
-                    filemode='a')
-
-with open(json_path, 'r') as file:
-    content = json.load(file)
-    logging.info('Data writed to a variable')
-
+                    filemode='w')
+conn = None
 try: 
-    conn = psycopg2.connect(
+    with psycopg2.connect(
         dbname='postgres',
         user='postgres',
         password='admin',
         host='localhost'
-    )
+    ) as conn:
 
-    logging.info('Connection Sucessful!')
+        logging.info('Connection Successful!')
+        with conn.cursor() as cur:
+            cur.execute
 
-    cur = conn.cursor()
-    for item in content:
-        values_insert = (
-            item['id'],
-            item['title'],
-            item['price'],
-            item['description'],
-            item['category'],
-            item['image'],
-            json.dumps(item['rating'])
-        )
-        cur.execute("""INSERT INTO 
-                        bronze(id,title,price,description,category,image,rating)
-                        VALUES
-                        (%s,%s,%s,%s,%s,%s,%s)
-                        ON CONFLICT (id) DO UPDATE SET 
-                        title = EXCLUDED.title, 
-                        price = EXCLUDED.price,
-                        description = EXCLUDED.description,
-                        category = EXCLUDED.category,
-                        image = EXCLUDED.image""",values_insert)
-    conn.commit()
-    cur.close()
-    conn.close()
-except Exception as e:
-    logging.error(f'Connection Failed! {e}')
+finally:
+    if conn:
+        conn.close()
+    else:
+        logging.error('Connection Failed')
